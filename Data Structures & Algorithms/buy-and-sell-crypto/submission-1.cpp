70b8eea400de893529ab102cf2888d5487eb4d53
May Saban <maysa7485@gmail.com>
2026-09-14 10:27:02 +0300

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,39 @@
+//#include <cmath>
+
+class Solution 
+{
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int maxPro = 0;
+        int currProfit = 0;
+        int minPrice = prices[0];
+
+        int left = 0;
+        int right = 0;
+
+        for (int i = 0; i < prices.size(); ++i)
+        {
+            if (prices[i] < minPrice)
+            {
+                minPrice = prices[i];
+                left = right;
+                currProfit = 0;
+            }
+
+            else
+            {
+                currProfit = prices[right] - prices[left];
+
+                if (currProfit > maxPro)
+                {
+                    maxPro = currProfit;
+                }
+            }
+
+            ++right;
+        }
+
+        return maxPro;
+    }
+};
